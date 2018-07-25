d9b0ca8bd9f406f53cc05fc103cd32aa4527a73c
Nashla Yerlitte <Nashla Yerlitte>
2018-07-25 00:26:11 -0500

    Initial commit

---
+++ app/src/main/java/com/example/win8/foodtemplate/Actividades/CompartirActivity.java
@@ -0,0 +1,15 @@
+package com.example.win8.foodtemplate.Actividades;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.win8.foodtemplate.R;
+
+public class CompartirActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compartir);
+    }
+}
---
+++ app/src/main/java/com/example/win8/foodtemplate/Actividades/EncontrarRestaurantActivity.java
@@ -0,0 +1,15 @@
+package com.example.win8.foodtemplate.Actividades;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.win8.foodtemplate.R;
+
+public class EncontrarRestaurantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_encontrar_restaurant);
+    }
+}
